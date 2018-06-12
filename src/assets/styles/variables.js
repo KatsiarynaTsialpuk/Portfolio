9d0342fbaa1567b0c28ad3765cc6aff314843f34
Katia <katya@spiralscout.com>
2018-06-13 01:13:34 +0300

    D finish about page

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,7 +1,9 @@
 module.exports = {
-    white: "#efeee9",
-    beige: "#eeebdf",
+    lighter: "#fcfcfc",
+    white: "#ffffff",
+    beige: "#EFEBE0",
     green: "#00bfa5",
+    "dark-gray": "#6E7171",
     "dark-green": "#009688",
     black: "#000000",
     "text-color": "#455a64",
