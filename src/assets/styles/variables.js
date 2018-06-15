30dcd145222fe1c87c938cab4df3bfa0099ae932
Katia <katya@spiralscout.com>
2018-06-15 10:12:02 +0300

    D add 1/2 part of the works page

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -2,6 +2,7 @@ module.exports = {
     lighter: "#fcfcfc",
     white: "#ffffff",
     "light-gray": "#efeee9",
+    "light-yellow": "#fbf8f0",
     beige: "#EFEBE0",
     green: "#00bfa5",
     "dark-gray": "#6E7171",
