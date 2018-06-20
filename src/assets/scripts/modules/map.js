ee7ac55fa3de3045fde8c03b926a542cc337b61b
Katia <katya@spiralscout.com>
2018-06-20 09:25:59 +0300

    D finish Works-page

--- src/assets/scripts/modules/map.js
+++ src/assets/scripts/modules/map.js
@@ -1,11 +1,111 @@
+//Google maps
+var map;
+var center = {
+  lat: 53.917742,
+  lng: 27.478588
+};
+
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
-    });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: center,
+    zoom: 14,
+    zoomControl: true,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_CENTER
+    },
+    streetViewControl: true,
+    streetViewControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER
+    },
+    styles: [
+      {
+        featureType: "administrative",
+        elementType: "geometry",
+        stylers: [
+          {
+            visibility: "on"
+          }
+        ]
+      },
+      {
+        featureType: "administrative",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#444444"
+          }
+        ]
+      },
+      {
+        featureType: "landscape",
+        elementType: "all",
+        stylers: [
+          {
+            color: "#f2f2f2"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: -100
+          },
+          {
+            lightness: 45
+          }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "simplified"
+          }
+        ]
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "labels.icon",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "transit",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "all",
+        stylers: [
+          {
+            color: "#2ecfc1"
+          },
+          {
+            visibility: "on"
+          }
+        ]
+      }
+    ]
+  });
 }
+google.maps.event.addDomListener(window, "load", initMap);
--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,14 +1,14 @@
 module.exports = {
-    lighter: "#fcfcfc",
-    white: "#ffffff",
-    "light-gray": "#efeee9",
-    "light-yellow": "#fbf8f0",
-    beige: "#EFEBE0",
-    green: "#00bfa5",
-    "dark-gray": "#6E7171",
-    "dark-green": "#009688",
-    black: "#000000",
-    "text-color": "#455a64",
-    grey: "#373e42",
-    "light-grey": "#dfdcd5"
+  lighter: "#fcfcfc",
+  white: "#ffffff",
+  "light-gray": "#efeee9",
+  "light-yellow": "#fbf8f0",
+  beige: "#EFEBE0",
+  green: "#00bfa5",
+  "dark-gray": "#6E7171",
+  "dark-green": "#009688",
+  black: "#000000",
+  "text-color": "#455a64",
+  grey: "#373e42",
+  "light-grey": "#dfdcd5"
 };
